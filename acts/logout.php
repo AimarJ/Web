befab447abb1f991d2743a48f841bafc06e6cdd8
AimarJ <aimar.kauts@khk.ee>
2017-05-30 18:21:33 +0300

    Väljalogimisfail

---
+++ acts/logout.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aimar.kauts
+ * Date: 3.05.2017
+ * Time: 18:00
+ */
+$sess->delsession(); //kustutame olemasoleva sessiooni
+$http->redirect(); //suuname avalehele
