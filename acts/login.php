6611777225170e209728cd2dfdad9f9ed525f94e
AimarJ <aimar.kauts@khk.ee>
2017-06-05 13:18:46 +0300

    muudetud faili sisu

--- acts/login.php
+++ acts/login.php
@@ -10,9 +10,9 @@ $login = new template('login');
 $error = $sess->get('error');
 $login->set('error', $error);
 // paneme reaalsed väärtused template elementidele
-$login->set('kasutajanimi', 'Kasutaja');
-$login->set('parool', 'Parool');
-$login->set('nupp', 'Logi sisse');
+$login->set('kasutajanimi', tr('Kasutaja'));
+$login->set('parool', tr('Parool'));
+$login->set('nupp', tr('Logi sisse'));
 // loome link sisselogimisvormi töötlusele
 $link = $http->getLink(array('act' => 'login_do'));
 $login->set('link', $link);
